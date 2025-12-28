c457187f0f846fabdf7e40c143d22a22c8db3f6b
LiseAndreasen <lise.andreasen.1967@gmail.com>
2025-12-28 15:43:13 +0100

    code prettier

--- 2024/d17a.php
+++ 2024/d17a.php
@@ -34,7 +34,10 @@ while($inspoi < $prgend) {
 	$operand = $prg[$inspoi+1];
 
 /*
-There are two types of operands; each instruction specifies the type of its operand. The value of a literal operand is the operand itself. For example, the value of the literal operand 7 is the number 7. The value of a combo operand can be found as follows:
+There are two types of operands; each instruction specifies the type of its 
+operand. The value of a literal operand is the operand itself. For example, 
+the value of the literal operand 7 is the number 7. The value of a combo 
+operand can be found as follows:
 
     Combo operands 0 through 3 represent literal values 0 through 3.
     Combo operand 4 represents the value of register A.
@@ -61,25 +64,36 @@ There are two types of operands; each instruction specifies the type of its oper
 			break;
 	}
 
-/*The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.*/
+/*The adv instruction (opcode 0) performs division. The numerator is the 
+value in the A register. The denominator is found by raising 2 to the power 
+of the instruction's combo operand. (So, an operand of 2 would 
+divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of
+the division operation is truncated to an integer and then written to the
+A register.*/
 
 	if($opcode == 0) {
 		$a = (int) ($a / pow(2, $combo));
 	}
 
-/*The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.*/
+/*The bxl instruction (opcode 1) calculates the bitwise XOR of register B 
+and the instruction's literal operand, then stores the result in register B.*/
 
 	if($opcode == 1) {
 		$b = $b ^ $operand;
 	}
 
-/*The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.*/
+/*The bst instruction (opcode 2) calculates the value of its combo operand 
+modulo 8 (thereby keeping only its lowest 3 bits), then writes that value 
+to the B register.*/
 
 	if($opcode == 2) {
 		$b = $combo % 8;
 	}
 
-/*The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.*/
+/*The jnz instruction (opcode 3) does nothing if the A register is 0. 
+However, if the A register is not zero, it jumps by setting the instruction 
+pointer to the value of its literal operand; if this instruction jumps, the 
+instruction pointer is not increased by 2 after this instruction.*/
 
 	if($opcode == 3) {
 		if($a != 0) {
@@ -89,13 +103,17 @@ There are two types of operands; each instruction specifies the type of its oper
 	}
 
 
-/*The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)*/
+/*The bxc instruction (opcode 4) calculates the bitwise XOR of register B 
+and register C, then stores the result in register B. (For legacy reasons, 
+this instruction reads an operand but ignores it.)*/
 
 	if($opcode == 4) {
 		$b = $b ^ $c;
 	}
 
-/*The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)*/
+/*The out instruction (opcode 5) calculates the value of its combo operand 
+modulo 8, then outputs that value. (If a program outputs multiple values, 
+they are separated by commas.)*/
 
 	if($opcode == 5) {
 		$tmp = $combo % 8;
@@ -106,13 +124,17 @@ There are two types of operands; each instruction specifies the type of its oper
 	}
 
 
-/*The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)*/
+/*The bdv instruction (opcode 6) works exactly like the adv instruction 
+except that the result is stored in the B register. (The numerator is still 
+read from the A register.)*/
 
 	if($opcode == 6) {
 		$b = (int) ($a / pow(2, $combo));
 	}
 
-/*The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)*/
+/*The cdv instruction (opcode 7) works exactly like the adv instruction 
+except that the result is stored in the C register. (The numerator is still 
+read from the A register.)*/
 
 	if($opcode == 7) {
 		$c = (int) ($a / pow(2, $combo));
