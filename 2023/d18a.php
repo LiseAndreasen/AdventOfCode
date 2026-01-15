434be2e3330f6a54292b3585bfaefd2fb151ea27
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-01-15 03:50:41 +0100

    part 1 works, with sparse graph

--- 2023/d18a.php
+++ 2023/d18a.php
@@ -3,7 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////
 // constants
 
-$input = file_get_contents('./d18input1.txt', true);
+$input = file_get_contents('./d18input2.txt', true);
 
 ///////////////////////////////////////////////////////////////////////////
 // functions
@@ -18,73 +18,85 @@ function get_input($input) {
 	return $data;
 }
 
-function print_map($map, $min_x, $min_y) {
-	$map_width = sizeof($map);
-	$map_height = sizeof($map[0]);
-	for($j=$min_y;$j<$min_y+$map_height;$j++) {
-		for($i=$min_x;$i<$min_x+$map_width;$i++) {
+function print_map($map, $data_x, $data_y) {
+	foreach($data_y as $j) {
+		foreach($data_x as $i) {
 			echo $map[$i][$j];
 		}
 		echo "\n";
 	}
-	for($i=0;$i<$map_width;$i++) {
+	for($i=0;$i<max($data_x)-min($data_x);$i++) {
 		echo "=";
 	}
 	echo "\n";
 }
 
-function remake_map($min_x, $min_y) {
-	global $map;
-	$map2 = $map;
-	$map_width = sizeof($map);
-	$map_height = sizeof($map[0]);
-	
-	// assumption: this loop is nice
-	/*
-	| is a vertical pipe connecting north and south.
-	- is a horizontal pipe connecting east and west.
-	L is a 90-degree bend connecting north and east.
-	J is a 90-degree bend connecting north and west.
-	7 is a 90-degree bend connecting south and west.
-	F is a 90-degree bend connecting south and east.
-	*/	
-	for($j=$min_y;$j<$min_y+$map_height;$j++) {
-		for($i=$min_x;$i<$min_x+$map_width;$i++) {
-			if($map[$i][$j] == "#") {
-				// | ?
-				if(isset($map[$i][$j-1]) && $map[$i][$j-1] == "#" && isset($map[$i][$j+1]) && $map[$i][$j+1] == "#") {
-					$map2[$i][$j] = "|";
-					continue;
+// which character represents this corner of the lagoon?
+function corner_char($old_dir, $dig_dir) {
+	if($old_dir == "U" && $dig_dir == "L") {
+		return "7";
+	}
+	if($old_dir == "U" && $dig_dir == "R") {
+		return "F";
+	}
+	if($old_dir == "D" && $dig_dir == "L") {
+		return "J";
+	}
+	if($old_dir == "D" && $dig_dir == "R") {
+		return "L";
+	}
+	if($old_dir == "L" && $dig_dir == "U") {
+		return "L";
+	}
+	if($old_dir == "L" && $dig_dir == "D") {
+		return "F";
+	}
+	if($old_dir == "R" && $dig_dir == "U") {
+		return "J";
+	}
+	if($old_dir == "R" && $dig_dir == "D") {
+		return "7";
+	}
+}
+
+// given a map, mark the internal parts of it
+function fill_in_map(&$map, $data_x, $data_y) {
+	// fill in map with empty bits
+	foreach($data_y as $y) {
+		foreach($data_x as $x) {
+			if(!isset($map[$x][$y])) {
+				$map[$x][$y] = ".";
+			}
+		}
+	}
+
+	foreach($data_y as $j) {
+		$tiles_left = 0;
+		foreach($data_x as $i) {
+			if($map[$i][$j] != ".") {
+				if($map[$i][$j] == "|") {
+					$tiles_left++;
+				} else {
+					// the logic here is, that starting + ending above the line
+					// gives a part of the loop, that can be ignored
+					// similar for starting and ending below
+					// this part starts above or ends below the line
+					if($map[$i][$j] == "L" || $map[$i][$j] == "7") {
+						$tiles_left += 0.5;
+					} else {
+						// the opposite
+						if($map[$i][$j] == "F" || $map[$i][$j] == "J") {
+							$tiles_left += - 0.5;
+						}
+					}
 				}
-				// - ?
-				if(isset($map[$i-1][$j]) && $map[$i-1][$j] == "#" && isset($map[$i+1][$j]) && $map[$i+1][$j] == "#") {
-					$map2[$i][$j] = "-";
-					continue;
-				}
-				// L ?
-				if(isset($map[$i][$j-1]) && $map[$i][$j-1] == "#" && isset($map[$i+1][$j]) && $map[$i+1][$j] == "#") {
-					$map2[$i][$j] = "L";
-					continue;
-				}
-				// J ?
-				if(isset($map[$i-1][$j]) && $map[$i-1][$j] == "#" && isset($map[$i][$j-1]) && $map[$i][$j-1] == "#") {
-					$map2[$i][$j] = "J";
-					continue;
-				}
-				// 7 ?
-				if(isset($map[$i-1][$j]) && $map[$i-1][$j] == "#" && isset($map[$i][$j+1]) && $map[$i][$j+1] == "#") {
-					$map2[$i][$j] = "7";
-					continue;
-				}
-				// F ?
-				if(isset($map[$i+1][$j]) && $map[$i+1][$j] == "#" && isset($map[$i][$j+1]) && $map[$i][$j+1] == "#") {
-					$map2[$i][$j] = "F";
-					continue;
+			} else {
+				if(abs($tiles_left) % 2 == 1) {
+					$map[$i][$j] = "I";
 				}
 			}
 		}
 	}
-	$map = $map2;
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -93,106 +105,157 @@ function remake_map($min_x, $min_y) {
 $data = get_input($input);
 //print_r($data);
 
-// construct map of dig
 $x = 0;
 $y = 0;
-$min_x = $x;
-$max_x = $x;
-$min_y = $y;
-$max_y = $y;
-$map[$x][$y] = "#";
+// make list of x and y actually used, including neighbs
 foreach($data as $dig) {
 	$dig_dir = $dig[0];
 	$dig_lgt = $dig[1];
+	if($dig_dir == "L") {
+		$x -= $dig_lgt;
+	}
+	if($dig_dir == "R") {
+		$x += $dig_lgt;
+	}
+	if($dig_dir == "U") {
+		$y -= $dig_lgt;
+	}
+	if($dig_dir == "D") {
+		$y += $dig_lgt;
+	}
+	$data_x[$x-1] = $x-1;
+	$data_x[$x] = $x;
+	$data_x[$x+1] = $x+1;
+	$data_y[$y-1] = $y-1;
+	$data_y[$y] = $y;
+	$data_y[$y+1] = $y+1;
+}
+sort($data_x);
+sort($data_y);
+
+// construct map of dig
+$x = 0;
+$y = 0;
+$map[$x][$y] = "*"; // will be fixed later
+$old_dir = "*"; // undefined
+foreach($data as $dig) {
+	$dig_dir = $dig[0];
+	$dig_lgt = $dig[1];
+	if($old_dir != "*") {
+		$map[$x][$y] = corner_char($old_dir, $dig_dir);
+	}
 	for($i=0;$i<$dig_lgt;$i++) {
 		switch($dig_dir) {
 			case "L":
 				$x--;
+				$key_of_i = array_search($x, $data_x);
+				if($key_of_i === false) {
+					// this i isn't interesting
+					// skip to the next
+					// the one just before it WAS interesting
+					$key_of_prev_i = array_search($x + 1, $data_x);
+					$this_x = $data_x[$key_of_prev_i - 1];
+					$diff = $x - $this_x;
+					$x = $this_x;
+					$i += $diff;
+				}
 				break;
 			case "R":
 				$x++;
+				$key_of_i = array_search($x, $data_x);
+				if($key_of_i === false) {
+					// this i isn't interesting
+					// skip to the next
+					// the one just before it WAS interesting
+					$key_of_prev_i = array_search($x - 1, $data_x);
+					$this_x = $data_x[$key_of_prev_i + 1];
+					$diff = $this_x - $x;
+					$x = $this_x;
+					$i += $diff;
+				}
 				break;
 			case "U":
 				$y--;
+				$key_of_i = array_search($y, $data_y);
+				if($key_of_i === false) {
+					// this i isn't interesting
+					// skip to the next
+					// the one just before it WAS interesting
+					$key_of_prev_i = array_search($y + 1, $data_y);
+					$this_y = $data_y[$key_of_prev_i - 1];
+					$diff = $y - $this_y;
+					$y = $this_y;
+					$i += $diff;
+				}
 				break;
 			case "D":
 				$y++;
+				$key_of_i = array_search($y, $data_y);
+				if($key_of_i === false) {
+					// this i isn't interesting
+					// skip to the next
+					// the one just before it WAS interesting
+					$key_of_prev_i = array_search($y - 1, $data_y);
+					$this_y = $data_y[$key_of_prev_i + 1];
+					$diff = $this_y - $y;
+					$y = $this_y;
+					$i += $diff;
+				}
 				break;
 		}
-		$map[$x][$y] = "#";
-		if($x < $min_x) {
-			$min_x = $x;
-		}
-		if($max_x < $x) {
-			$max_x = $x;
-		}
-		if($y < $min_y) {
-			$min_y = $y;
-		}
-		if($max_y < $y) {
-			$max_y = $y;
-		}
-	}
-}
-
-// fill in map with empty bits
-for($x=$min_x;$x<=$max_x;$x++) {
-	for($y=$min_y;$y<=$max_y;$y++) {
-		if(!isset($map[$x][$y])) {
-			$map[$x][$y] = ".";
-		}
-	}
-}
-
-//print_map($map, $min_x, $min_y);
-remake_map($min_x, $min_y);
-//print_map($map, $min_x, $min_y);
-
-// heavily copy from day 10
-
-$map_width = sizeof($map);
-$map_height = sizeof($map[0]);
-
-// go through map, testing whether each tile is
-// 1) not part of loop
-// 2) goes through the loop an odd number of times coming from the left
-
-for($j=$min_y;$j<$max_y;$j++) {
-	$tiles_left = 0;
-	for($i=$min_x;$i<$max_x;$i++) {
-		if($map[$i][$j] != ".") {
-			if($map[$i][$j] == "|") {
-				$tiles_left++;
-			} else {
-				// the logic here is, that starting + ending above the line
-				// gives a part of the loop, that can be ignored
-				// similar for starting and ending below
-				// this part starts above or ends below the line
-				if($map[$i][$j] == "L" || $map[$i][$j] == "7") {
-					$tiles_left += 0.5;
-				} else {
-					// the opposite
-					if($map[$i][$j] == "F" || $map[$i][$j] == "J") {
-						$tiles_left += - 0.5;
-					}
-				}
-			}
+		if($i == $dig_lgt - 1) {
+			$map[$x][$y] = "*"; // will be fixed later
 		} else {
-			if(abs($tiles_left) % 2 == 1) {
-				$map[$i][$j] = "I";
+			if($dig_dir == "U" || $dig_dir == "D") {
+				$map[$x][$y] = "|";
+			} else {
+				$map[$x][$y] = "-";
 			}
 		}
 	}
+	$old_dir = $dig_dir;
 }
-//print_map($map, $min_x, $min_y);
+// fix the very first bit
+$dig_dir = $data[0][0];
+$map[$x][$y] = corner_char($old_dir, $dig_dir);
+
+// changes $map
+fill_in_map($map, $data_x, $data_y);
 
 // count lava parts of map
-$flat_map = array_merge(...$map);
-$counts = array_count_values($flat_map);
-//print_r($counts);
-
-$all_map = sizeof($flat_map);
-$lava_map = $all_map - $counts["."];
+$lava_map = 0;
+$prev_x = min($data_x) - 1;
+$all_chars = max($data_x) - min($data_x) + 1;
+foreach($data_x as $x) {
+	if($x != $prev_x + 1) {
+		// there are missing columns
+		// add them anyway
+		// how many columns?
+		$diff = $x - $prev_x - 1;
+		// these are well defined from the previous loop!
+		$lava_map += $lava_chars * $diff;
+	}
+	$prev_y = min($data_y) - 1;
+	$lava_chars = 0;
+	foreach($data_y as $y) {
+		$char = $map[$x][$y];
+		if($y != $prev_y + 1) {
+			// there are missing rows
+			// add them anyway
+			// how many rows?
+			$diff = $y - $prev_y - 1;
+			if($char != ".") {
+				$lava_chars += $diff;
+			}
+		}
+		if($char != ".") {
+			$lava_chars++;
+		}
+		$prev_y = $y;
+	}
+	$lava_map += $lava_chars;
+	$prev_x = $x;
+}
 
 printf("Size of lava lagoon: %d\n", $lava_map);
 
