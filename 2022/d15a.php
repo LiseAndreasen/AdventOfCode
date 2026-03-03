d635ac64682e660f8aa6e1d5b0e7dcb72a769b79
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-03-03 10:36:39 +0100

    code works, part 1

--- 2022/d15a.php
+++ 2022/d15a.php
@@ -3,7 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////
 // constants
 
-$input_example = false; // example or input?
+$input_example = true; // example or input?
 
 if($input_example) {
     $input = file_get_contents('./d15input2.txt', true);
@@ -29,18 +29,30 @@ function get_input($input) {
 	return $data;
 }
 
-function print_map($map) {
-    foreach($map[0] as $j => $cell) {
-        printf("%3d ", $j);
-        foreach($map as $i => $col) {
-            echo $map[$i][$j];
-        }
-        echo "\n";
+// Source - https://stackoverflow.com/a/3631016
+// Posted by Matthew
+// Retrieved 2026-03-03, License - CC BY-SA 2.5
+function merge_data($data) {
+    usort($data, function($a, $b)
+    {
+        return $a[0] - $b[0];
+    });
+    
+    $n = 0; $len = count($data);
+    for ($i = 1; $i < $len; ++$i)
+    {
+        if ($data[$i][0] > $data[$n][1] + 1)
+            $n = $i;
+            else
+            {
+                if ($data[$n][1] < $data[$i][1])
+                    $data[$n][1] = $data[$i][1];
+                    unset($data[$i]);
+            }
     }
-    for($i=0;$i<sizeof($map);$i++) {
-        echo "=";
-    }
-    echo "\n";
+    
+    $data = array_values($data);
+    return $data;
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -49,6 +61,7 @@ function print_map($map) {
 $data = get_input($input);
 
 // figure out size of map
+// begin constructing the test row
 foreach ($data as $sensor_beacon) {
     [$x1, $y1, $x2, $y2] = $sensor_beacon;
     $dist = abs($x2 - $x1) + abs($y2 - $y1);
@@ -56,6 +69,13 @@ foreach ($data as $sensor_beacon) {
     $all_high_xs[] = $x1 + $dist;
     $all_low_ys[] = $y1 - $dist;
     $all_high_ys[] = $y1 + $dist;
+    
+    if($y1 == $test_row) {
+        $test_row_ranges_keep[] = [$x1, $x1, "S"];
+    }
+    if($y2 == $test_row) {
+        $test_row_ranges_keep[] = [$x2, $x2, "B"];
+    }
 }
 
 $xmin = min($all_low_xs);
@@ -63,46 +83,37 @@ $xmax = max($all_high_xs);
 $ymin = min($all_low_ys);
 $ymax = max($all_high_ys);
 
-// create map of sensors and beacons
-$map_col = array_fill($ymin, $ymax - $ymin + 1, ".");
-$map = array_fill($xmin, $xmax - $xmin + 1, $map_col);
+// a sensor at x1, y1 at a distance dist from the beacon
+// will also cover all these ranges, where a + b = dist
+// 0 < a, b
+// (x1 - a, y1 - b) -> (x1 + a, y1 - b)
+// (x1 - a, y1 + b) -> (x1 + a, y1 + b)
 
-foreach ($data as $sensor_beacon) {
-    [$x1, $y1, $x2, $y2] = $sensor_beacon;
-    $map[$x1][$y1] = "S";
-    $map[$x2][$y2] = "B";
-}
-
-// fill in map with places beacons can't be
-$map2 = $map;
-
+// continue work on the test row
 foreach ($data as $sensor_beacon) {
     [$x1, $y1, $x2, $y2] = $sensor_beacon;
     $dist = abs($x2 - $x1) + abs($y2 - $y1);
-    // i is the x coordinate, travelling from -dist to dist
-    for($i=-$dist;$i<=$dist;$i++) {
-        // j is the y coordinate
-        for($j=-($dist-abs($i));$j<=$dist-abs($i);$j++) {
-            $map2[$x1+$i][$y1+$j] = "#";
-        }
+    // this area goes from y1 - dist -> y1 + dist
+    if($y1 - $dist <= $test_row && $test_row <= $y1 + $dist) {
+        $b = abs($test_row - $y1);
+        $a = $dist - $b;
+        $test_row_ranges[] = [$x1 - $a, $x1 + $a, "#"];
     }
 }
 
-// add the existing sensors and beacons back in
-foreach ($data as $sensor_beacon) {
-    [$x1, $y1, $x2, $y2] = $sensor_beacon;
-    $map2[$x1][$y1] = "S";
-    $map2[$x2][$y2] = "B";
+// among the ranges, it is important never to delete sensors and beacons
+// assumption: that any sensors and beacons are within other ranges
+$test_row_ranges_keep = merge_data($test_row_ranges_keep);
+$test_row_ranges = merge_data($test_row_ranges);
+
+$length_of_ranges = 0;
+foreach ($test_row_ranges_keep as $trr) {
+    $length_of_ranges -= $trr[1] - $trr[0] + 1;
+}
+foreach ($test_row_ranges as $trr) {
+    $length_of_ranges += $trr[1] - $trr[0] + 1;
 }
 
-// testing a single row
-$no_beacons_here = 0;
-foreach ($map2 as $col) {
-    if($col[$test_row] == "#") {
-        $no_beacons_here++;
-    }
-}
-
-printf("Result 1: %d\n", $no_beacons_here);
+printf("Result 1: %d\n", $length_of_ranges);
 
 ?>
